ee100eff6db3ef6b4cbfb4704a8a6ab66ef40fe4
HeydariADI <adiheydari2@gmail.com>
2026-06-13 14:14:46 +0330

    update  auth middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,15 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  function middleware(req) {
+    return;
+  },
+  {
+    pages: {
+      signIn: "/authentication/login",
+    },
+  }
+);
 
 export const config = {
   matcher: ["/dashboard/:path*"],
