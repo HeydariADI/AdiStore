513ac1128464874f8ec0de19f9331831a06393a3
HeydariADI <adiheydari2@gmail.com>
2026-06-29 16:13:00 +0330

    fix:error

--- src/app/api/products/[slug]/route.js
+++ src/app/api/products/[slug]/route.js
@@ -2,45 +2,18 @@ import { NextResponse } from "next/server";
 import { connectToDatabase } from "@lib/mongodb";
 import Products from "@models/Products";
 
-
 export const runtime = "nodejs";
 
 export async function GET(req, context) {
-  try {
-    await connectToDatabase();
+  console.log("==========");
+  console.log("context:", context);
+  console.log("params:", context?.params);
+  console.log("slug:", context?.params?.slug);
+  console.log("==========");
 
-    const slug = context.params?.slug;
-
-    console.log("🔥 SLUG:", slug);
-
-    if (!slug) {
-      return NextResponse.json(
-        { message: "slug نامعتبر است" },
-        { status: 400 }
-      );
-    }
-
-    const product = await Products.findOne({
-      slug: slug.trim().toLowerCase(),
-    }).lean();
-
-    if (!product) {
-      return NextResponse.json(
-        { message: "محصول یافت نشد" },
-        { status: 404 }
-      );
-    }
-
-    return NextResponse.json({
-      ...product,
-      _id: product._id.toString(),
-    });
-  } catch (err) {
-    console.error("PRODUCT API ERROR:", err);
-
-    return NextResponse.json(
-      { message: "server error" },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json({
+    context,
+    params: context?.params,
+    slug: context?.params?.slug,
+  });
 }
