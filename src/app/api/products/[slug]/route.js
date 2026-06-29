a09ef005fa0bd9083c5a3e847dc0e3d02fb20048
HeydariADI <adiheydari2@gmail.com>
2026-06-29 16:17:45 +0330

    fix:error

--- src/app/api/products/[slug]/route.js
+++ src/app/api/products/[slug]/route.js
@@ -4,16 +4,43 @@ import Products from "@models/Products";
 
 export const runtime = "nodejs";
 
-export async function GET(req, context) {
-  console.log("==========");
-  console.log("context:", context);
-  console.log("params:", context?.params);
-  console.log("slug:", context?.params?.slug);
-  console.log("==========");
+export async function GET(request, { params }) {
+  try {
+    await connectToDatabase();
 
-  return NextResponse.json({
-    context,
-    params: context?.params,
-    slug: context?.params?.slug,
-  });
+    // Next.js 16
+    const { slug } = await params;
+
+    console.log("🔥 SLUG:", slug);
+
+    if (!slug) {
+      return NextResponse.json(
+        { message: "slug نامعتبر است" },
+        { status: 400 }
+      );
+    }
+
+    const product = await Products.findOne({
+      slug: slug.trim().toLowerCase(),
+    }).lean();
+
+    if (!product) {
+      return NextResponse.json(
+        { message: "محصول یافت نشد" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({
+      ...product,
+      _id: product._id.toString(),
+    });
+  } catch (err) {
+    console.error("PRODUCT API ERROR:", err);
+
+    return NextResponse.json(
+      { message: "server error" },
+      { status: 500 }
+    );
+  }
 }
